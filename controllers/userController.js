2b1d8a3953a8a6bbb470e9c1598b7f2f4fe53132
andz89m <andzrivero89@gmail.com>
2022-12-11 19:41:30 -0800

    add validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,53 +1,48 @@
-const User = require('../models/User')
+const User = require("../models/User");
 
-exports.login = async (req, res) => {
-    let user = new User(req.body);
-  let xx =  await  user.login()
-    console.log(xx);
-    if(xx.length > 0) {
-        req.session.user = {
-            email: user.data.username
-        }
-        res.redirect('/')
-
-    }else{
-        req.flash('errors', 'wrong password')
-        res.redirect('/')
-    }
-    
-
+exports.login = (req, res) => {
+  let user = new User(req.body);
+  user
+    .login()
+    .then((data) => {
+      req.session.user = {
+        email: data[0].email,
+      };
+      res.redirect("/");
+    })
+    .catch((err) => {
+      req.flash("errors", err);
+      res.redirect("/");
+    });
 };
 exports.logout = function (req, res) {
-    req.session.destroy();
-    res.redirect('/')
-}
+  req.session.destroy();
+  res.redirect("/");
+};
 exports.home = (req, res) => {
-    if (req.session.user) {
-        res.render('home-dashboard', {email: req.session.user.email})
-
-    } else {
-        res.render('home-guest', {
-            errors: req.flash('errors'),
-            regErrors: req.flash('regErrors')
-        })
-
-    }
-}
+  if (req.session.user) {
+    res.render("home-dashboard", { email: req.session.user.email });
+  } else {
+    res.render("home-guest", {
+      errors: req.flash("errors"),
+      regErrors: req.flash("regErrors"),
+    });
+  }
+};
 exports.register = (req, res) => {
-    let user = new User(req.body)
-    user.register().then(() => {
-        req.session.user = {
-            username: user.data.username
-        };
-        res.redirect('/')
-
-    }).catch((regErrors) => {
-
-        regErrors.forEach(error => {
-            req.flash('regErrors', error)
-        });
-        res.redirect('/')
-
+  let user = new User(req.body);
+  user
+    .register()
+    .then(() => {
+      req.session.user = {
+        email: user.data.email,
+      };
+      res.redirect("/");
     })
-
-}
+    .catch((regErrors) => {
+      regErrors.forEach((error) => {
+        req.flash("regErrors", error);
+      });
+      res.redirect("/");
+    });
+};
--- models/User.js
+++ models/User.js
@@ -1,125 +1,165 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
+const db = require("../db");
 
-const db = require('../db')
-
-
-let validator = require('validator');
+let validator = require("validator");
 
 let User = function (data) {
-    this.data = data;
-    this.errors = []
+  this.data = data;
+  this.errors = [];
+  this.errors_data = {};
 };
-
 
 User.prototype.cleanUp = function () {
-    if (typeof(this.data.username) != 'string') {
-        this.data.username = ""
-    };
-    if (typeof(this.data.email) != 'string') {
-        this.data.email = ""
-    };
-    if (typeof(this.data.password) != 'string') {
-        this.data.password = ""
-    };
+  if (typeof this.data.username != "string") {
+    this.data.username = "";
+  }
+  if (typeof this.data.email != "string") {
+    this.data.email = "";
+  }
+  if (typeof this.data.password != "string") {
+    this.data.password = "";
+  }
 
-    this.data = {
-        username: this.data.username.trim().toLowerCase(),
-        email: this.data.email.trim().toLowerCase(),
-        password: this.data.password
-    }
-
+  this.data = {
+    username: this.data.username.trim().toLowerCase(),
+    email: this.data.email.trim().toLowerCase(),
+    password: this.data.password,
+  };
 };
 User.prototype.validate = function () {
-    return new Promise(async (resolve, reject) => { // if inputs are empty
+  return new Promise((resolve, reject) => {
+    // if inputs are empty
 
-        if (this.data.username === "") {
-            this.errors.push('You must provide username.')
-        };
-        if (this.data.username != "" && ! validator.isAlphanumeric(this.data.username)) {
-            this.errors.push('username can only contain numbers and letters.')
-        };
-        if (! validator.isEmail(this.data.email)) {
-            this.errors.push('You must provide a valid email address.')
-        };
-        if (this.data.password === "") {
-            this.errors.push('You must provide a password.')
-        };
+    if (this.data.username === "") {
+      //   this.errors.push("You must provide username.");
+      this.errors_data.username = "You must provide username.";
+    }
+    if (
+      this.data.username != "" &&
+      !validator.isAlphanumeric(this.data.username)
+    ) {
+      //   this.errors.push("username can only contain numbers and letters.");
+      this.errors_data.username =
+        "username can only contain numbers and letters.";
+    }
+    if (!validator.isEmail(this.data.email)) {
+      //   this.errors.push("You must provide a valid email address.");
+      this.errors_data.email = "You must provide a valid email address.";
+    }
+    if (this.data.password === "") {
+      //   this.errors.push("You must provide a password.");
+      this.errors_data.password = "You must provide a password.";
+    }
 
-        // length of input password
-        if (this.data.password.length > 0 && this.data.password.length < 12) {
-            this.errors.push("you must povide atleast 12 characters in your password");
+    // length of input password
+    if (this.data.password.length > 0 && this.data.password.length < 5) {
+      //   this.errors.push(
+      //     "you must povide atleast 12 characters in your password"
+      //   );
+      this.errors_data.password =
+        "you must povide atleast 12 characters in your password.";
+    }
+    if (this.data.password.length > 30) {
+      //   this.errors.push("Password cannot exceed 30 characters");
+      this.errors_data.password = "Password cannot exceed 30 characters";
+    }
+    // length of input username
+    if (this.data.username.length > 0 && this.data.username.length < 3) {
+      //   this.errors.push("you must povide atleast 3 characters in your username");
+      this.errors_data.username =
+        "username can only contain numbers and letters.";
+    }
+    if (this.data.username.length > 30) {
+      this.errors.push("username cannot exceed 30 characters");
+    }
+
+    // Only if username and email is valid then check to see if it's already taken
+    const checkExistUsername = () => {
+      return new Promise((resolve, reject) => {
+        if (
+          this.data.username.length > 2 &&
+          this.data.username.length < 31 &&
+          validator.isAlphanumeric(this.data.username)
+        ) {
+          let sql = `SELECT * FROM users WHERE name = "${this.data.username}" OR email = "${this.data.email}"`;
+
+          db.query(sql, (err, result) => {
+            if (err) {
+              reject(err);
+              return false;
+            }
+
+            resolve(result);
+          });
         }
-        if (this.data.password.length > 30) {
-            this.errors.push("Password cannot exceed 30 characters");
+      });
+    };
+
+    checkExistUsername().then((result) => {
+      if (result.length) {
+        console.log(result);
+        if (result[0].email === this.data.email) {
+          this.errors.push("That email is already taken.");
+          resolve();
         }
-        // length of input username
-        if (this.data.username.length > 0 && this.data.username.length < 3) {
-            this.errors.push("you must povide atleast 3 characters in your username");
+        if (result[0].name === this.data.username) {
+          this.errors.push("That username is already taken.");
+          resolve();
         }
-        if (this.data.username.length > 30) {
-            this.errors.push("username cannot exceed 30 characters");
-        }
-
-
-        // // Only if username is valid then check to see if it's already taken
-        // if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
-        //     let usernameExists = await usersCollection.findOne({username: this.data.username})
-        //     if (usernameExists) {
-        //         this.errors.push("That username is already taken.")
-        //     }
-        // }
-
-        // // Only if email is valid then check to see if it's already taken
-        // if (validator.isEmail(this.data.email)) {
-        //     let emailExists = await usersCollection.findOne({email: this.data.email})
-        //     if (emailExists) {
-        //         this.errors.push("That email is already being used.")
-        //     }
-        // }
-        resolve()
-    })
-}
-
+      } else {
+        resolve();
+      }
+    });
+  });
+};
 
 User.prototype.login = function () {
+  this.cleanUp();
+  return new Promise((resolve, reject) => {
+    let sql = `SELECT * FROM users WHERE name = "${this.data.username}"`;
 
-    this.cleanUp()
-    return (new Promise((resolve, reject) => {
- 
-    let sql = `SELECT * FROM users WHERE password = "${this.data.password}"`
     db.query(sql, (err, result) => {
-        if (err) {
-            reject(err)
-            return false;
-        }
-    
-        resolve(result);
-    })
-}))
+      if (err) {
+        reject(err);
+        return false;
+      }
 
-}
+      if (
+        result &&
+        bcrypt.compareSync(this.data.password, result[0].password)
+      ) {
+        resolve(result);
+      } else {
+        reject("wrong password");
+      }
+    });
+  });
+};
 
 User.prototype.register = function () {
-    return new Promise(async (resolve, reject) => {
-        this.cleanUp();
-        await this.validate();
-        if (!this.errors.length) {
-            let salt = bcrypt.genSaltSync(10)
-            this.data.password = bcrypt.hashSync(this.data.password, salt);
-            let data = {
-                name: this.data.username,
-                email: this.data.email,
-                password: this.data.password
-            }
-            let sql = 'INSERT INTO users SET ?';
-            db.query(sql, data)
-            resolve();
-        } else {
-            reject(this.errors);
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    await this.validate();
+    if (!this.errors.length) {
+      let salt = bcrypt.genSaltSync(10);
+      this.data.password = bcrypt.hashSync(this.data.password, salt);
+      let data = {
+        name: this.data.username,
+        email: this.data.email,
+        password: this.data.password,
+      };
+      let sql = "INSERT INTO users SET ?";
+      db.query(sql, data, (err, result) => {
+        if (err) {
+          reject(err);
+          return false;
         }
-    })
-
-
-}
+        resolve(result);
+      });
+    } else {
+      reject(this.errors);
+    }
+  });
+};
 module.exports = User;
