21851264625dec557dff0d37ac8a523daa1654c6
andz89m <andzrivero89@gmail.com>
2023-05-25 06:07:34 -0800

    fixing login page

--- public/javascript/public-script/script.js
+++ public/javascript/public-script/script.js
@@ -1,38 +1,34 @@
- 
- 
 import Update from "./update.js";
- 
+
 import Loader from "./loader.js";
- 
+
 import Templates from "./templates.js";
- 
- 
+import Check from "./check.js";
+
+let check = new Check();
+check.check_password();
 let templates = new Templates();
 
 //certificate
-templates.template_event()
-templates.sumbmit_certificate_code()
+templates.template_event();
+templates.sumbmit_certificate_code();
 
 //invitation
-templates.invitation()
+templates.invitation();
 
 //user templates
-templates.my_templates()
-
+templates.my_templates();
 
 let update = new Update();
 update.update_account();
-update.modal_update_account()
+update.modal_update_account();
 let loader = new Loader();
 loader.loading();
-loader.loading_generate()
+loader.loading_generate();
 
-
-window.addEventListener('click', (e) =>{
-         //close alert success 
-         if(e.target.classList.contains('alert-close')){
-           e.target.parentElement.style.display = 'none'
-         
-          }
-
-})
+window.addEventListener("click", (e) => {
+  //close alert success
+  if (e.target.classList.contains("alert-close")) {
+    e.target.parentElement.style.display = "none";
+  }
+});
